21705c1bba6944fcb15defe5522d736f5b72d348
AlgoClassWork <kyxec.play@gmail.com>
2025-04-19 13:41:42 +0600

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created']
+    search_fields = ['title', 'body']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'post', 'created']
+    search_fields = ['name', 'body']

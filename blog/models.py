0a3b011c9924a243db73de3c8b46bcf709961e7c
AlgoClassWork <kyxec.play@gmail.com>
2025-04-19 13:09:29 +0600

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+

86eb0bdf12889f91389f88776bd8f1675576fcb0
AlgoClassWork <kyxec.play@gmail.com>
2025-04-19 13:41:54 +0600

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,14 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Комментарий от {self.name}'

43dc99c4c0fa1c7e1c68e168587a0fd1f8b2cd86
AlgoClassWork <kyxec.play@gmail.com>
2025-04-20 14:04:44 +0600

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    comments = Comment.objects.filter(post=post)
+    
+    comment_form = CommentForm()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = CommentForm()
+
+    return render(request, 'blog/detail.html', {
+                            'post':post,
+                            'comments':comments,
+                            'comment_form':comment_form})

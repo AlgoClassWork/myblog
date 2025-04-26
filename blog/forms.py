e30b4a08f248d6d19d2c71bc8ab5703e149c480d
AlgoClassWork <kyxec.play@gmail.com>
2025-04-26 12:40:11 +0600

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name','body']
+
+class EmailForm(forms.ModelForm):
+    name = forms.CharField(max_length=20)
+    to = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea, required=False)

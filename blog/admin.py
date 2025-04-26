0a94eaed418d672b3117e033a3dcf4794bc94f44
AlgoClassWork <kyxec.play@gmail.com>
2025-04-26 13:50:41 +0600

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,26 @@
-from django.contrib import admin
-from .models import Post, Comment
+# Настройки админ-панели, чтобы управлять постами, комментариями и лайками
 
+from django.contrib import admin
+from .models import Post, Comment, Like
+
+# Регистрируем посты
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'created']
-    search_fields = ['title', 'body']
+    list_display = ['title', 'author', 'created', 'like_count']  # Добавляем лайки
+    search_fields = ['title', 'body']                           # Поиск по заголовку и тексту
 
+    def like_count(self, obj):
+        return obj.like_count()  # Показываем количество лайков
+    like_count.short_description = 'Лайки'  # Название столбца
 
+# Регистрируем комментарии
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'post', 'created']
-    search_fields = ['name', 'body']
+    list_display = ['name', 'post', 'created']    # Что показывать
+    search_fields = ['name', 'body']              # Поиск по имени и тексту
+
+# Регистрируем лайки
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['post', 'ip_address', 'created']  # Показываем пост и IP
+    search_fields = ['ip_address']                   # Поиск по IP
